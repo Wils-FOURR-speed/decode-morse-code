c465602f9216a7c1f5c5b52a2c07b38df569c75b
Wils-FOURR-speed <124159862+Wils-FOURR-speed@users.noreply.github.com>
2023-09-27 16:15:56 +0100

    Add morse code

---
+++ decode_morse_code.rb
@@ -0,0 +1,43 @@
+class MorseCodeDecoder
+  MORSE_CODE = {
+    '.-' => 'A',
+    '-...' => 'B',
+    '-.-.' => 'C',
+    '-..' => 'D',
+    '.' => 'E',
+    '..-.' => 'F',
+    '--.' => 'G',
+    '....' => 'H',
+    '..' => 'I',
+    '.---' => 'J',
+    '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
+  }.freeze
+
+  def self.decode_char(code)
+    MORSE_CODE[code]
+  end
+
+  def self.decode_word(code)
+    code.split.map { |char| decode_char(char) }.join
+  end
+
+  def self.decode_message(code)
+    words = code.split('   ')
+    words.map { |word| decode_word(word) }.join(' ')
+  end
+end
